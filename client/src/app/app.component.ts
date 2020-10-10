c02e33eaebe2831a953ab9e8d28ea465c96f7958
BrandyKun <55144997+BrandyKun@users.noreply.github.com>
2020-10-10 05:49:30 +0100

    Start of mobile View

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,18 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IPhoto } from './shared/models/photo';
 import { IPagination } from './shared/models/pagination';
-
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'Photobook website';
-  
+
   constructor() {}
 
   ngOnInit(): void {
+    $(document).ready(function () {
+      $('.burgermenu').on('click', function () {
+        $('.mob-nav').fadeToggle(400);
+      });
+    });
   }
 }
---
+++ client/src/assets/scripts/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.burgermenu').on('click', function () {
+        $('.mob-nav').fadeToggle(400);
+    })
+});
+
+$(function() {
+    alert('Hello, custom js');
+});
