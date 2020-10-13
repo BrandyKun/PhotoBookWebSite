157f4ca29bee74d700422c27d8619256d3cb24e3
BrandyKun <55144997+BrandyKun@users.noreply.github.com>
2020-10-13 13:36:11 +0100

    Added the UI Error Handling

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { NgxUploaderModule } from 'ngx-uploader';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { AlbumModule } from './album/album.module';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 import { PhotoEditorComponent } from './photo-editor/photo-editor.component';
 import { HomeModule } from './home/home.module';
 
@@ -20,9 +20,12 @@ import { HomeModule } from './home/home.module';
     NgxUploaderModule,
     HttpClientModule,
     CoreModule,
-    HomeModule
+    HomeModule, 
+    
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
