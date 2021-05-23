2e54f4065a84ea36e3f2f7d139a7399408e586f5
BrandyKun <55144997+BrandyKun@users.noreply.github.com>
2021-05-23 15:39:34 +0100

    deployment chages

--- API/wwwroot/assets/scripts/custom.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.burgermenu').on('click', function () {
-        $('.mob-nav').fadeToggle(400);
-    })
-});
-
-// $(function() {
-//     alert('Hello, custom js');
-// });
