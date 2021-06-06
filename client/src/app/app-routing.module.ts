2b3cd839fab2f7d1a66477a82816c485250206a3
BrandyKun <55144997+BrandyKun@users.noreply.github.com>
2021-06-06 11:36:30 +0100

    basket changes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { ErrorPageComponent } from './core/error-page/error-page.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { TestErrorComponent } from './core/test-error/test-error.component';
 import { ServerErrorComponent } from './core/server-error/server-error.component';
 import { NotFoundComponent } from './core/not-found/not-found.component';
 import { AuthGuard } from './core/guards/auth.guard';
-import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
 import { AlbumComponent } from './album/album.component';
 import { AboutEditComponent } from './admin/about-edit/about-edit.component';
 
@@ -20,18 +18,12 @@ const routes: Routes = [
   { path: 'photos', loadChildren: () => import('./album/album.module').then(mod => mod.AlbumModule)},
   { path: 'home', loadChildren: () => import('./home/home.module').then(mod => mod.HomeModule)},
   { path: 'account', loadChildren: () => import('./account/account.module').then(mod => mod.AccountModule)},
+  { path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule)},
   { path: 'admin',
     runGuardsAndResolvers: 'always',
     canActivate:[AuthGuard], 
     loadChildren: () => import('./admin/admin.module').then(mod => mod.AdminModule)},
-  // { path: 'admin', component: AdminPanelComponent},
-  // { path: 'admin-panel', component: AdminPanelComponent},
   { path: 'edit-about', component: AboutEditComponent},
-  
-  // { path: '',
-  //   runGuardsAndResolvers: 'always',
-  //   canActivate: [AuthGuard],
-  //   children: [{ path: 'admin', loadChildren: () => import('./admin/admin.module').then(mod => mod.AdminModule)}]},
   { path: '', redirectTo: '', pathMatch: 'full' },
 ];
 
--- client/src/app/shared/models/photo.ts
+++ client/src/app/shared/models/photo.ts
@@ -1,6 +1,7 @@
 export interface IPhoto {
     id: number;
     url: string;
+    price: number;
     description: string;
     dateAdded: Date;
     tag: string;
